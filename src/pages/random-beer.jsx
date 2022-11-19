9e12a82c528cbc60d346c20b85e73589beb8e8d3
montibel <113550254+montibel@users.noreply.github.com>
2022-11-19 23:03:24 +0100

    randombeer ok

--- src/pages/random-beer.jsx
+++ src/pages/random-beer.jsx
@@ -1,11 +1,39 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function RandomBeer() {
+  const [beer, setBeers] = useState({});
+  const { id } = useParams();
+  // This will run only once after the initial render
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then((response) => {
+        console.log("response.data", response.data);
+        setBeers(response.data);
+      });
+  }, [id]);
+
   return (
     <div>
-      <Navbar />
-      <h1>Random Beer</h1>
+      <img src={beer.image_url} alt="singlebeer" />
+      <h3>{beer.name}</h3>
+
+      <p>
+        <b>{beer.tagline}</b>
+      </p>
+      <p>{beer.description}</p>
+      <p>
+        <b>First brewed:</b> {beer.first_brewed}
+      </p>
+      <p>
+        <b>Attenuation level:</b> {beer.attenuation_level}
+      </p>
+
+      <p>
+        <b>Contributed by {beer.contributed_by}</b>
+      </p>
     </div>
   );
 }
